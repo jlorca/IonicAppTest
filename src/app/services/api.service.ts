6c2c2438ef25984cdacc4d913e2377e0422ad1cb
Jesus Lorca Luque <jeslorlu@gmail.com>
2018-10-30 15:43:01 +0100

    add header to the request

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -11,16 +11,17 @@ export class ApiService {
 
     constructor(private http: HttpClient) {
         this.baseUrl = AppConstant.BACKEND.BASE_URL;
-        this.httpOptions = {
-            headers: new HttpHeaders({
-                'Content-Type':  'application/json',
-                'sf_user': 'jlorca.community01@gmail.com',
-                'sf_password': 'lorkytest00oNIOW0hZrMAppnk8IBasYuI7s'
-            })
-        };
     }
 
     findAllAccounts(): Observable<AccountDTO[]> {
-        return this.http.put<AccountDTO[]>(this.baseUrl + '/accounts', this.httpOptions);
+        const httpHeaders = new HttpHeaders()
+        .set("Content-Type","application/json")
+        .set("sf_user","jlorca.community01@gmail.com")
+        .set("sf_password","lorkytest00oNIOW0hZrMAppnk8IBasYuI7s");
+        const httpOptions = {
+            headers: httpHeaders
+        };
+
+        return this.http.get<AccountDTO[]>(this.baseUrl + '/accounts', httpOptions);
     }
 }
